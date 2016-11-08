d171e2a21fc725b91f80da082672376a4b589fe3
Anil Ramakrishna <anil.k.ramakrishna@gmail.com>
2016-11-08 13:21:46 -0800

    Working code

--- get_speaker_counts.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd ../Data
-
-for i in `find ../Data/utterances_with_charnames/ -type f -exec basename {} \;`; do
-    echo "$i"
-    cat "utterances_with_charnames/$i" | awk -F '=>' '{ print $1 }' | sort | uniq -c | sort -r > "speaker_counts/$i"
-done
--- run_all.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf ../Data/scripts_txt/*
-rm -rf ../Data/utterances_with_charnames/*
-rm -rf ../Data/speaker_counts/*
-./a_01_fetch_extract_and_parse.py True
-./get_speaker_counts.sh
