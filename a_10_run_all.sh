cc3cd6037700a82f7e5a8bfa20f35c85f5f7a4fc
Anil Ramakrishna <anil.k.ramakrishna@gmail.com>
2016-11-08 13:23:52 -0800

    Bug fixed

--- a_10_run_all.sh
+++ a_10_run_all.sh
@@ -4,4 +4,4 @@ rm -rf ../Data/scripts_txt/*
 rm -rf ../Data/utterances_with_charnames/*
 rm -rf ../Data/speaker_counts/*
 ./a_01_fetch_extract_and_parse.py True
-./get_speaker_counts.sh
+./a_02_get_speaker_counts.sh
